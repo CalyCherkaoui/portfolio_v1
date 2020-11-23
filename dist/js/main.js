3715c1efea2b8932c7e516f21180c9e428f2ea39
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-11-23 09:11:06 -0600

    Scrolling animation

--- dist/js/main.js
+++ dist/js/main.js
@@ -10,29 +10,6 @@ gsap.utils.toArray(".panel").forEach((panel, i) => {
   });
 });
 
-ScrollTrigger.create({
-  // snap: 1 / 3
-});
-
-
-// gsap.registerPlugin(ScrollTrigger);
-
-// let sections = gsap.utils.toArray(".panel");
-
-// gsap.to(sections, {
-//   yPercent: -100 * (sections.length - 1),
-//   ease: "none",
-//   scrollTrigger: {
-//     trigger: "#home",
-//     pin: true,
-//     scrub: 1,
-//     snap: 1 / (sections.length - 1),
-//     // base vertical scrolling on how wide the container is so it feels more natural.
-//     end: () => "+=" + document.querySelector("#home").offsetHeight
-//   }
-// });
-
 // ScrollTrigger.create({
-// //   // snap: 1 / 8 // snap whole page to the closest section!
-//   pinSpacing: false
+//   snap: 1 / 3
 // });
